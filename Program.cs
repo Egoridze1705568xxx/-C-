c7593fe834f73455e91dd06902be1d2460c0883d
Egor <levikkk1705568@yandex.ru>
2023-05-31 12:56:04 +0300

    Сделал задачу

--- Program.cs
+++ Program.cs
@@ -1,37 +1,13 @@
-﻿/* Задача 21
-Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-A (3,6,8); B (2,1,-7), -> 15.84
-A (7,-5, 0); B (1,-1,9) -> 11.53
+﻿
+/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
 */
-
-/*double NumberX1 = double.Parse(Console.ReadLine());
-double NumberY1 = double.Parse(Console.ReadLine());
-double NumberZ1 = double.Parse(Console.ReadLine());
-
-double NumberX2 = double.Parse(Console.ReadLine());
-double NumberY2 = double.Parse(Console.ReadLine());
-double NumberZ2 = double.Parse(Console.ReadLine());
-
-
-double NumberX = (NumberX2 - NumberX1) * (NumberX2 - NumberX1);
-double NumberY = (NumberY2 - NumberY1) * (NumberY2 - NumberY1);
-double NumberZ = (NumberZ2 - NumberZ1) * (NumberZ2 - NumberZ1);
-double NumberSum = NumberX + NumberY + NumberZ;
-
-double Range = (Math.Sqrt(NumberSum));
-Console.WriteLine (Range);
-*/
-
-/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-3 -> 1, 8, 27
-5 -> 1, 8, 27, 64, 125
-*/
-
 /*
-int number = int.Parse(Console.ReadLine());
+double number_a = double.Parse(Console.ReadLine());
+double number_b = double.Parse(Console.ReadLine());
 
-for (int i = 1; i < number + 1; i++){
-    Console.Write(Math.Pow(i,3));
-    Console.Write(" ");
-}
+double result = Math.Pow(number_a, number_b);
+Console.WriteLine(result);
 */
+
