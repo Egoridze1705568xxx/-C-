3321958b6141268e6a0e5577a72e18afa251e08c
Egor <levikkk1705568@yandex.ru>
2023-06-13 23:27:11 +0300

    записал двухмерный массив

--- Program.cs
+++ Program.cs
@@ -1,13 +1,43 @@
-﻿
-/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-3, 5 -> 243 (3⁵)
-2, 4 -> 16
-*/
-/*
-double number_a = double.Parse(Console.ReadLine());
-double number_b = double.Parse(Console.ReadLine());
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-double result = Math.Pow(number_a, number_b);
-Console.WriteLine(result);
-*/
 
+Console.Write("Введите колличество строк: ");
+double m = double.Parse(Console.ReadLine());
+Console.Write("Введите колличество столбцов: ");
+double n = double.Parse(Console.ReadLine());
+
+
+
+double[,] array =  GenereteRandom2DArray(3, 4);
+
+PrintArray(array);
+
+void PrintArray(double[,] array )
+{
+for (var i = 0; i < array.GetLength(0); i++)
+{
+    for (var j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+double[,] GenereteRandom2DArray(double m, double n)
+{
+    double [,] array = new double [(int)m, (int)n];
+
+    Random random = new Random();
+
+    for (var i = 0; i < m; i++)
+    {
+        for (var j = 0; j < n; j++)
+        {
+            array [i,j] = random.Next(-10,10);
+            //Console.Write($"({i},{j})");
+        }
+        //Console.WriteLine();
+    }
+    return array;
+}
