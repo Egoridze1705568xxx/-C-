15985944991ebf9a3589930639f8830d4bd3c9a5
Egor <levikkk1705568@yandex.ru>
2023-06-16 04:36:28 +0300

    Решил задачу 60

--- Program.cs
+++ Program.cs
@@ -1,32 +1,39 @@
-﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+﻿//Задача 60. ...Сформируйте трёхмерный массив из  двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+//Массив размером 2 x 2 x 2
 
+/*
 
 Console.Write("Введите колличество строк: ");
 double m = double.Parse(Console.ReadLine());
 Console.Write("Введите колличество столбцов: ");
 double n = double.Parse(Console.ReadLine());
+Console.Write("Введите колличество столбцов: ");
+double x = double.Parse(Console.ReadLine());
 
 
-
-double[,] array =  GenereteRandom2DArray(m, n);
+double[, ,] array =  GenereteRandom2DArray(m, n, x);
 
 PrintArray(array);
 
-void PrintArray(double[,] array )
+void PrintArray(double[, ,] array )
 {
 for (var i = 0; i < array.GetLength(0); i++)
 {
     for (var j = 0; j < array.GetLength(1); j++)
     {
-        Console.Write($"{array[i,j]} ");
+       for (int z = 0; z < array.GetLength(2); z++)
+       {
+        
+       }
+       // Console.Write($"{array[i,j]} ");
     }
     Console.WriteLine();
 }
 }
 
-double[,] GenereteRandom2DArray(double m, double n)
+double[, ,] GenereteRandom2DArray(double m, double n, double x)
 {
-    double [,] array = new double [(int)m, (int)n];
+    double [, ,] array = new double [(int)m, (int)n, (int)x];
 
     Random random = new Random();
 
@@ -34,12 +41,18 @@ double[,] GenereteRandom2DArray(double m, double n)
     {
         for (var j = 0; j < n; j++)
         {
-            array [i,j] = random.Next(-10,10);
-            //Console.Write($"({i},{j})");
+                for (int z = 0; z < x; z++)
+                {
+                    array [i,j,z] = random.Next(0,100);
+                    Console.Write($"{array[i,j,z]} ");
+                    Console.Write($"({i},{j},{z}) ");
+
+                }
+        
         }
-        //Console.WriteLine();
+        Console.WriteLine();
     }
     return array;
 }
 
-
+*/
